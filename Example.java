6e7fe116fe1f98aa5a9b92ada64db03f290bbbd1
DarshanaJK <128844702+DarshanaJK@users.noreply.github.com>
2024-01-02 14:50:19 +0530

    Create Notepad

--- Example.java
+++ Example.java
@@ -1,110 +1,77 @@
 import javax.swing.*;
 import java.awt.*;
-class AddCustomerForm extends JFrame{
+class Notepad extends JFrame{
 
-    JLabel titleLable;
-    JButton addButton, cancelButton;
+    TextArea textArea;
 
-    JLabel idLabel;
-    JLabel nameLabel;
-    JLabel addressLabel;
-    JLabel salaryLabel;
+    JMenuBar mainMenuBar;
 
-    JTextField idTextField;
-    JTextField nameTextField;
-    JTextField addressTextField;
-    JTextField salaryTextField;
+    JMenu fileMenu;
+    JMenu editMenu;
+    JMenu viewMenu;
 
-    AddCustomerForm(){
-        setSize(400,400);
-        setTitle("AddCustomerForm");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JMenuItem item1;
+    JMenuItem item2;
+    JMenuItem item3;
+    JMenuItem item4;
+    JMenuItem item5;
+    JMenuItem item6;
+
+
+    Notepad(){
+        setSize(600, 500);
+        setTitle("Untitled");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setLayout(new BorderLayout());
 
-        titleLable = new JLabel("Add Customer Form");
-        titleLable.setFont(new Font("", Font.BOLD, 35));
-        titleLable.setHorizontalAlignment(JLabel.CENTER);
-        add("North", titleLable);
-
-        JPanel centerJPanel = new JPanel(new GridLayout(4,2));
-
-
-        idLabel = new JLabel("ID : ");
-        idLabel.setFont(new Font("", Font.BOLD, 15));
-        JPanel idLabelPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        idLabelPanel.add(idLabel);
-        centerJPanel.add(idLabelPanel);
-
-        idTextField = new JTextField(5);
-        idTextField.setFont(new Font("", Font.BOLD, 15));
-        JPanel idTextPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        idTextPanel.add(idTextField);
-        centerJPanel.add(idTextPanel);
+        mainMenuBar = new JMenuBar();
 
 
 
-        nameLabel = new JLabel("Name : ");
-        nameLabel.setFont(new Font("", Font.BOLD, 15));
-        JPanel nameLabelPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        nameLabelPanel.add(nameLabel);
-        centerJPanel.add(nameLabelPanel);
+        fileMenu = new JMenu("File");
+        item1 = new JMenuItem("Menu Item 1");
+        item2 = new JMenuItem("Menu Item 2");
+        fileMenu.add(item1);
+        fileMenu.add(item2);
+        mainMenuBar.add(fileMenu);
 
-        nameTextField = new JTextField(12);
-        nameTextField.setFont(new Font("", Font.BOLD, 15));
-        JPanel nameTextPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        nameTextPanel.add(nameTextField);
-        centerJPanel.add(nameTextPanel);
+        editMenu = new JMenu("Edit");
+        item3 = new JMenuItem("Menu Item 3");
+        item4 = new JMenuItem("Menu Item 4");
+        editMenu.add(item3);
+        editMenu.add(item4);
+        mainMenuBar.add(editMenu);
 
-
-
-        addressLabel = new JLabel("Address : ");
-        addressLabel.setFont(new Font("", Font.BOLD, 15));
-        JPanel addressLabelPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        addressLabelPanel.add(addressLabel);
-        centerJPanel.add(addressLabelPanel);
-
-        addressTextField = new JTextField(15);
-        addressTextField.setFont(new Font("", Font.BOLD, 15));
-        JPanel addressTextPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        addressTextPanel.add(addressTextField);
-        centerJPanel.add(addressTextPanel);
+        viewMenu = new JMenu("View");
+        item5 = new JMenuItem("Menu Item 5");
+        item6 = new JMenuItem("Menu Item 6");
+        viewMenu.add(item5);
+        viewMenu.add(item6);
+        mainMenuBar.add(viewMenu);
 
 
 
-        salaryLabel = new JLabel("Salary : ");
-        salaryLabel.setFont(new Font("", Font.BOLD, 15));
-        JPanel salaryLabelPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        salaryLabelPanel.add(salaryLabel);
-        centerJPanel.add(salaryLabelPanel);
-
-        salaryTextField = new JTextField(6);
-        salaryTextField.setFont(new Font("", Font.BOLD, 15));
-        JPanel salaryTextPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        salaryTextPanel.add(salaryTextField);
-        centerJPanel.add(salaryTextPanel);
-
-        add("Center", centerJPanel);
+        setJMenuBar(mainMenuBar);
 
 
+        textArea = new TextArea();
+        textArea.setFont(new Font("", Font.BOLD, 16));
 
-        addButton = new JButton("Add");
-        addButton.setFont(new Font("", Font.BOLD, 16));
+        add("Center", textArea);
 
-        cancelButton = new JButton("Cancel");
-        cancelButton.setFont(new Font("", Font.BOLD, 16));
 
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        buttonPanel.add(addButton);
-        buttonPanel.add(cancelButton);
-
-        add("South", buttonPanel);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        add("Center",scrollPane);
 
         setVisible(true);
     }
+
+
+
 }
+
 class Example{
     public static void main(String[] args) {
-        new AddCustomerForm();
+        new Notepad();
     }
 }
